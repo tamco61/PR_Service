11e8c7516d74ef09d0f535b279221ebbcd3bddfd
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-11-14 00:51:16 +0300

    main init

--- cmd/main,.go
+++ cmd/main,.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	routes "app/internal/api"
+	"app/internal/api/handler"
 	"app/internal/config"
 	"app/internal/db"
 	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -12,6 +16,17 @@ func main() {
 	db, err := db.InitDB(cfg.DBPath)
 	if err != nil {
 		log.Fatal("failed to connect database:", err)
+	}
+
+	userHandler := handler.NewUserHandler(db)
+	teamHandler := handler.NewTeamHandler(db)
+	pullrequestHandler := handler.NewPullRequestHandler(db)
+
+	r := gin.Default()
+	routes.RegisterRoutes(r, teamHandler, userHandler, pullrequestHandler)
+
+	if err := r.Run(cfg.ServerAddr); err != nil {
+		log.Fatal("failed to run server:", err)
 	}
 
 }
