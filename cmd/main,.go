491a0a4966b55c4bc6d14e971579319f30f95d4e
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-11-14 00:48:34 +0300

    сокращение логики

--- cmd/main,.go
+++ cmd/main,.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"app/internal/config"
+	"app/internal/db"
+	"log"
+)
+
 func main() {
+	cfg := config.Load()
+
+	db, err := db.InitDB(cfg.DBPath)
+	if err != nil {
+		log.Fatal("failed to connect database:", err)
+	}
 
 }
