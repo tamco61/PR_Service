f01ffb75d75bad6b36f97db4612c40bfc6349adc
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-11-13 23:57:14 +0300

    add endpoint in router

--- internal/api/router.go
+++ internal/api/router.go
@@ -7,5 +7,22 @@ import (
 )
 
 func RegisterRoutes(r *gin.Engine, teamHandler *handler.TeamHandler, userHandler *handler.UserHandler, pullrequestHandler *handler.PullRequestHandler) {
+	team := r.Group("/team")
+	{
+		team.GET("/team/get", teamHandler.Get)
+		team.POST("/team/add", teamHandler.Add)
+	}
 
+	users := r.Group("/users")
+	{
+		users.GET("/users/getReview", userHandler.GetReview)
+		users.POST("/users/setIsActive", userHandler.SetIsActive)
+	}
+
+	pullrequest := r.Group("/pullRequest")
+	{
+		pullrequest.POST("/pullRequest/create", pullrequestHandler.Create)
+		pullrequest.POST("/pullRequest/merge", pullrequestHandler.Merge)
+		pullrequest.POST("/pullRequest/reassign", pullrequestHandler.Reassign)
+	}
 }
