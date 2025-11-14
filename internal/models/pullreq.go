445a7b36ee8e89e65b3d4d51d60d560bea80fee1
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-11-14 19:33:06 +0300

    req + docker

--- internal/models/pullreq.go
+++ internal/models/pullreq.go
@@ -3,7 +3,7 @@ package models
 type PullRequest struct {
 	ID        uint   `gorm:"primaryKey"`
 	Name      string `gorm:"not null"`
-	Author    User   `gorm:"not null"`
+	AuthorID  uint   `gorm:"not null"`
 	Status    bool   `gorm:"default:false"`
 	Reviewers []User `gorm:"many2many:reviewers"`
 }
