cb0265b518ae9e098cfbf1bedc43b0c080b91c0c
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-11-15 20:16:24 +0300

    remove

--- internal/models/team.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Team struct {
-	ID    uint   `gorm:"primaryKey"`
-	Users []User `gorm:"many2many:team_users"`
-}
--- internal/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type User struct {
-	ID       uint   `gorm:"primaryKey"`
-	Name     string `gorm:"not null"`
-	IsActive bool   `gorm:"default:true"`
-}
