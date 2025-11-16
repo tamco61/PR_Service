ff06478b1b305a8434186fac0d69e57b356f7d88
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-11-16 18:28:55 +0300

    todo remove

--- internal/models/team.go
+++ internal/models/team.go
@@ -1,7 +1,5 @@
 package models
 
-// todo проверка по условию корректности моделей
-
 type Team struct {
 	TeamName string `gorm:"primaryKey"`
 	Members  []User `gorm:"many2many:team_users"`
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,7 +1,5 @@
 package models
 
-// todo проверка по условию корректности моделей
-
 type User struct {
 	ID       string `gorm:"primaryKey"`
 	Name     string `gorm:"not null"`
